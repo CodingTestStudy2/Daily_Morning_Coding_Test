d40ff65c3f118ed5478ebd6bccf438cc8929b6cd
jinyus <jinyhehe@gmail.com>
2024-07-17 10:51:48 +0900

    solve 2/2 정수삼각형

--- 프로그래머스/lv3/지준혁/정수 삼각형.cpp
+++ 프로그래머스/lv3/지준혁/정수 삼각형.cpp
@@ -1,17 +1,21 @@
-// 작성 중 시간...
 class Solution {
     
-    int dp[504][504];
     public int solution(int[][] triangle) {
         
+        int n = triangle.length;
+        int[][] dp = new int[n][n];
         dp[0][0] = triangle[0][0];
-        for (int i = 1; i <= n; ++i) {
+        for (int i = 1; i < n; ++i) {
             for (int j = 0; j <= i; ++j) {
                 if (j == 0) dp[i][j] = dp[i - 1][j] + triangle[i][j];
                 else if (j == i) dp[i][j] = dp[i - 1][j - 1] + triangle[i][j];
-                else 
+                else dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j];
             }
         }
-        return answer;
+        int ans = 0;
+        for (int j = 0; j < n; ++j) {
+            ans = Math.max(ans, dp[n - 1][j]);
+        }
+        return ans;
     }
 }

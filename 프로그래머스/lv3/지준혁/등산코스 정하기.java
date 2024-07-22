27ee831ad9c3d8ea1fd58130900535e01840b166
jinyus <jinyhehe@gmail.com>
2024-07-22 11:40:14 +0900

    solve 등산 코스 정하기

--- 프로그래머스/lv3/지준혁/등산코스 정하기.java
+++ 프로그래머스/lv3/지준혁/등산코스 정하기.java
@@ -1,9 +1,10 @@
-// 해결 X
+import java.util.*;
+
 class Solution {
     
-    List<int[]>[] adj = new ArrayList[50004];
     public int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
         
+        List<int[]>[] adj = new ArrayList[n + 1];
         for (int i = 0; i <= n; ++i) adj[i] = new ArrayList<>();
         
         for (int i = 0; i < paths.length; ++i) {
@@ -11,27 +12,49 @@ class Solution {
             adj[paths[i][1]].add(new int[]{paths[i][2], paths[i][0]});
         }
         
-        int[] answer = {};
-        return answer;
-    }
-    int [] dijsktra(int st, int n, int[] gates, int[] summits) {
         int[] d = new int[n + 1];
         Arrays.fill(d, Integer.MAX_VALUE);
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0]));
-        d[st] = 0;
         
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0]));
+        for (var gate : gates) {
+            pq.add(new int[] {0, gate});
+            d[gate] = 0;
+        }
+
         while (!pq.isEmpty()) {
             int[] cur = pq.poll();
             int curWeight = cur[0];
             int curNode = cur[1];
-            
+
             if (d[curNode] != curWeight) continue;
-            
+            if (isSummit(curNode, summits)) continue;
+
             for (var nxt : adj[curNode]) {
-                int nxtWeight = nxt[0];
-                int nxtNode = nxt[1];
-                int intensity = Math.max(d[curNode], nxt)
+                int newWeight = Math.max(curWeight, nxt[0]);
+                if (newWeight < d[nxt[1]]) {
+                    d[nxt[1]] = newWeight;
+                    pq.add(new int[] {newWeight, nxt[1]});
+                }
             }
         }
+
+        int minSummit = Integer.MAX_VALUE;
+        int minIntensity = Integer.MAX_VALUE;
+        Arrays.sort(summits);
+        for (var s : summits) {
+            if (d[s] < minIntensity) {
+                minIntensity = d[s];
+                minSummit = s;
+            } 
+        }
+
+        return new int[] {minSummit, minIntensity};
+    }
+    
+    boolean isSummit(int cur, int[] summits) {
+        for (var e : summits) {
+            if (cur == e) return true;
+        }
+        return false;
     }
 }

b745cec995e2604df636a444b7dced702fbd20b7
jinyus <jinyhehe@gmail.com>
2024-07-23 11:53:17 +0900

    resolve 외벽 점검

--- 프로그래머스/lv3/지준혁/외벽 점검.java
+++ 프로그래머스/lv3/지준혁/외벽 점검.java
@@ -1,8 +1,9 @@
 import java.util.*;
 
-// 1. 원형 쭉 펴주기(시계방향으로만 탐색 가능)
-// 2. dist 모든 순열
 public class Solution {
+    
+    int ans = Integer.MAX_VALUE;
+    
     public int solution(int n, int[] weak, int[] dist) {
         List<Integer> weaks = new ArrayList<>();
         
@@ -10,6 +11,41 @@ public class Solution {
         
         for (int i = 0; i < weak.length - 1; ++i) weaks.add(n + weak[i]);
         
-        return 0;
+        dfs(new ArrayList<>(), new boolean[dist.length], dist, weaks, weak.length);
+        
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+    
+    void dfs(List<Integer> cur, boolean[] isVisited, int[] dist, List<Integer> weaks, int w) {
+        
+        if (cur.size() == dist.length) {
+            for (int i = 0; i < w; ++i) {
+                int st = weaks.get(i);
+                int en = weaks.get(i + w - 1);
+                for (int j = 0; j < cur.size(); ++j) {
+                    st += cur.get(j);
+                    if (st >= en) {
+                        int t = j + 1;
+                        ans = Math.min(ans, j + 1);
+                        break;
+                    }
+                    for (int k = i + 1; k < i + w; ++k) {
+                        if (weaks.get(k) > st) {
+                            st = weaks.get(k);
+                            break;
+                        }
+                    }
+                }
+            }
+            return ;
+        }
+        for (int i = 0; i < dist.length; ++i) {
+            if (isVisited[i]) continue;
+            isVisited[i] = true;
+            cur.add(dist[i]);
+            dfs(cur, isVisited, dist, weaks, w);
+            isVisited[i] = false;
+            cur.remove(cur.size() - 1);
+        }  
     }
 }

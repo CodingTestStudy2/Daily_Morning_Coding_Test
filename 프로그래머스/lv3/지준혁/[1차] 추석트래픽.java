7c06c2ecf8aa8dfb3b3ca1b9d9f53d10a6bc47d5
jinyus <jinyhehe@gmail.com>
2024-08-01 12:29:11 +0900

    resolve 추석 트래픽

--- 프로그래머스/lv3/지준혁/[1차] 추석트래픽.java
+++ 프로그래머스/lv3/지준혁/[1차] 추석트래픽.java
@@ -1,48 +1,41 @@
+import java.util.*;
+
 class Solution {
-    
-    int range;
+                
     public int solution(String[] lines) {
-        
-        int n = lines.length;
-        int answer = 0;
-        int[] dp = new int[60 * 60 * 1000];
-        int[] starts = new int[n];
-        int[] ends = new int[n];
-        
-        int idx = 0;
-        for (var line : lines) {
-            int en = convertToMile(line);
-            int st = en - (range + 1);
-            dp[(int)st]++;
-            dp[(int)(en + 1)]--;
-            starts[idx] = st;
-            ends[idx++] = en;
+               
+        List<int[]> logs = new ArrayList<>();
+
+        for (String line : lines) {
+            int[] ret = convertToMile(line);
+            int en = ret[0];
+            int st = en - (ret[1] - 1);
+            logs.add(new int[]{st, 1});
+            logs.add(new int[]{en + 1000, -1});
         }
 
-        return answer;
+        logs.sort((a, b) -> Integer.compare(a[0], b[0]));
+
+        int ans = 0;
+        int count = 0;
+        for (var log : logs) {
+            count += log[1];
+            ans = Math.max(ans, count);
+        }
+
+        return ans;
     }
-    int convertToMile(String time) {
+    
+    int[] convertToMile(String time) {
         String[] split = time.split(" ");
         String[] times = split[1].split(":");
-        String[] mileAndSec = times[2].split("\\.");
         
-        String[] split2 = split[2].substring(0, split[2].length() - 1).split("\\.");
+        int range = (int)(Double.parseDouble(split[2].substring(0, split[2].length() - 1)) * 1000);
         
-
-        range += Integer.parseInt(split2[0]) * 1000;
-        if (split[2].contains(".")) {            
-            int num = Integer.parseInt(split2[1]);
-            if (split2[1].length() == 1) range += num * 100;
-            else if (split2[1].length() == 2) range += num * 10;
-            else range += num;
-        }
-
-    
         int hour = Integer.parseInt(times[0]);
         int minute = Integer.parseInt(times[1]);
-        int sec = Integer.parseInt(mileAndSec[0]);
-        int mile = Integer.parseInt(mileAndSec[1]);
+        int secAndMile = (int)(Double.parseDouble(times[2]) * 1000.0);
         
-        return hour * 60 * 60 * 1000 + minute * 60 * 1000 + sec * 1000 + mile;
+        return new int[]{hour * 60 * 60 * 1000 + minute * 60 * 1000 + (int)secAndMile, range};
     }
 }

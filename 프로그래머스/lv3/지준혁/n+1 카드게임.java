73a90cbaf6f5ed0e9118f4088269d3af9f1b4fb7
jinyus <jinyhehe@gmail.com>
2024-08-08 12:16:22 +0900

    resolve n+1 카드게임

--- 프로그래머스/lv3/지준혁/n+1 카드게임.java
+++ 프로그래머스/lv3/지준혁/n+1 카드게임.java
@@ -1,18 +1,69 @@
 import java.util.*;
 
 class Solution {
-    // dfs -> 매 선택마다 4가지 경우 (왼쪽 카드를 코인을 사용해 갖거나, 오른쪽 카드를 코인을 사용해 갖거나, 둘다 갖거나, 둘 다 갖지 않는 경우)
-    // 999개 -> 666개 남음 (최대 4^333 경우의 수 ) -> dfs는 안될 거 같다.
-    
-    // 뽑지 않았던 카드를 저장해두고, 숫자를 만들 수 없을 때 코인이 있다면 뽑지 않았던 카드 중에서 카드 뽑아 코인 소모하는 식으로 구현하면 되지 않을까?
     public int solution(int coin, int[] cards) {
         int n = cards.length;
         
-        List<Integer> mycards = new ArrayList<>();
-        for (int i = 0; i < n / 3; ++i) {
-            mycards.add(cards[i]);
+        int target = n + 1;
+        List<Integer> cardStack = new ArrayList<>();
+        List<Integer> hand = new ArrayList<>();
+        for (int i = 0; i < n / 3; ++i) hand.add(cards[i]);
+        
+        int cursor = n / 3; 
+        int ans = 1;
+        while (true) {
+            if (cursor < n) cardStack.add(cards[cursor++]);
+            if (cursor < n) cardStack.add(cards[cursor++]);
+
+            boolean isSearched = search(hand, target);
+            if (isSearched) {
+                ++ans;
+                continue;
+            }
+            
+            isSearched = false;
+            if (coin > 0) {
+                for (int i = 0; i < hand.size(); i++) {
+                    int find = target - hand.get(i);
+                    if (cardStack.contains(find)) {
+                        cardStack.remove(Integer.valueOf(find));
+                        coin--;
+                        isSearched = true;
+                        break;
+                    }
+                }
+            }  
+            if (isSearched) {
+                ++ans;
+                continue;
+            }
+
+            if (coin >= 2) {
+                isSearched = search(cardStack, target);
+                if (isSearched) {
+                    coin -= 2;
+                    ++ans;
+                    continue;
+                }
+            }
+
+            break;
         }
-                
-        return ans;
+
+        return Math.min(ans, n / 3 + 1);
+    }
+
+    boolean search(List<Integer> list, int target) {
+        for (int i = 0; i < list.size(); ++i) {
+            for (int j = i + 1; j < list.size(); ++j) {
+                if (list.get(i) + list.get(j) == target) {
+                    list.remove(j);
+                    list.remove(i);
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
+

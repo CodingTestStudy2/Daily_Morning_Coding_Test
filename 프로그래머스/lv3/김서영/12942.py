f1da759c4b94d2299d62423ed6a33dc1f7707c03
seoyeong200 <youngseogimkk@gmail.com>
2024-08-23 10:58:08 +0900

    fix lv3

--- 프로그래머스/lv3/김서영/12942.py
+++ 프로그래머스/lv3/김서영/12942.py
@@ -1,13 +1,12 @@
 def solution(matrix_sizes):
+    answer = 0
     n = len(matrix_sizes)
-    dp = [[10000 for _ in range(n)] for _ in range(n)]
+    dp = [[1e10 for _ in range(n)] for _ in range(n)]
     for i in range(n): dp[i][i] = 0
     for gap in range(1, n):
         for i in range(n - gap):
             j = i + gap
             for k in range(i, j):
-                print(f"gap:{gap} i:{i} j:{j}")
-                p = dp[i][k] + dp[k+1][j] + matrix_sizes[i-1][0] * matrix_sizes[k][0] * matrix_sizes[j][1]
-                print(dp, p)
-                if dp[i][j] > p: dp[i][j] = p
+                p = dp[i][k] + dp[k+1][j] + matrix_sizes[i][0] * matrix_sizes[k][1] * matrix_sizes[j][1]
+                dp[i][j] = min(dp[i][j], p)
     return dp[0][-1]

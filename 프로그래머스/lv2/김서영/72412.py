727f672cc29c74e9bde070d9b949c61a099ac88f
seoyeong200 <youngseogimkk@gmail.com>
2024-08-27 11:45:07 +0900

    fix lv2

--- 프로그래머스/lv2/김서영/72412.py
+++ 프로그래머스/lv2/김서영/72412.py
@@ -59,15 +59,67 @@ def solution_still_inefficient(info, query):
     
 # 와... 이래도 시간초과나네
 # 해싱해야될듯 근데 어케함?
+"""
+와.. 어케하냐면 
+
+언어/직군/경력/푸드를 걍 싹 연결해서 고유 키로 생성하고
+    (-인 경우 포함하기 위해 저 4개에서 - or not 즉 2^4 경우의 수로 키 생성해서 각 케이스에 점수 저장)
+    (이진트리니까 이거 생성할때 백트래킹.. 근데 4승이라 크게 의미없을듯)
+
+이걸로 info에 있는 애들 해싱해서 score를 value로 저장하고
+쿼리 돌면서 쿼리할 점수 찾으면 됨(이때 이진탐색)
+
+상상도 못한 정체다
+"""
 def solution(info, query):
-    dict = [
-        {'cpp': set(), 'java': set(), 'python': set()},
-        {'backend': set(), 'frontend': set()},
-        {'junior': set(), 'senior': set()},
-        {'chicken': set(), 'pizza': set()}
-    ]
-    for i in info:
-        lang, occu, career, food, score = i.split()
-        
+    from collections import defaultdict
+    from bisect import bisect_left
+
+    def generate_bit_mask() -> list:
+        bit_masks = []
+        def backtrack(size):
+            print(f"backtrack({size})")
+            if size == 4:
+                bit_masks.append(bit.copy())
+                return
+            bit.append(1)
+            backtrack(size+1)
+            bit[-1]=0
+            backtrack(size+1)
+            bit.pop()
+        bit=[]
+        backtrack(0)
+        return bit_masks
+    
+    bit_masks = [[1, 1, 1, 1], [1, 1, 1, 0], 
+                 [1, 1, 0, 1], [1, 1, 0, 0], 
+                 [1, 0, 1, 1], [1, 0, 1, 0], 
+                 [1, 0, 0, 1], [1, 0, 0, 0], 
+                 [0, 1, 1, 1], [0, 1, 1, 0], 
+                 [0, 1, 0, 1], [0, 1, 0, 0], 
+                 [0, 0, 1, 1], [0, 0, 1, 0], 
+                 [0, 0, 0, 1], [0, 0, 0, 0]]
+    
+    score_dict = defaultdict(list)
+    for info_ in info:
+        info_ = info_.split()
+        for bit_mask in bit_masks:
+            key = ""
+            for i in range(4):
+                key += info_[i] if bit_mask[i] else "-"
+            score_dict[key].append(int(info_[-1]))
+    print(score_dict)
+    
+    for _, v in score_dict.items(): v.sort()
+
+    answer = []
+    for q in query:
+        key, score = q.replace(" and ", "").split()
+        info_score = score_dict[key]
+        uncoditioned_num = bisect_left(info_score, int(score))
+        answer.append(len(info_score) - uncoditioned_num)
+    return answer
+
+    
     
     

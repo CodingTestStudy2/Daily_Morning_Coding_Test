3f105b3699b2acc32861aeca4795b3f20a368de7
oeyh98 <skusus123@gmail.com>
2024-08-27 10:27:56 +0900

    sovle 1/3

--- 프로그래머스/lv2/양진혁/양궁대회.py
+++ 프로그래머스/lv2/양진혁/양궁대회.py
@@ -3,22 +3,19 @@ from itertools import combinations_with_replacement
 
 def solution(n, info):
     answer = [-1]
-    lion_arrow = list(combinations_with_replacement(range(0, 11), n))
     max_diff = 0
 
-    for lion in lion_arrow:
-        lion = list(lion)
+    print(list(combinations_with_replacement(range(0, 3), 3)))
+    for i in list(combinations_with_replacement(range(0, 3), 3)):
         lion_score = 0
         apeach_score = 0
+        lion = [0] * 5
 
-        for k in range(0, 11):
-            if info[k] < lion[k]:
-                lion_score += (10 - k)
-            else:
-                apeach_score += (10 - k)
+        for j in i:
+            lion[j] += 1
 
-        if max_diff < lion_score - apeach_score:
-            max_diff = lion_score - apeach_score
-            answer = rion
+        print(lion)
+        lion[::-1]
+        print(lion)
 
-    return answer
+solution(5, [])

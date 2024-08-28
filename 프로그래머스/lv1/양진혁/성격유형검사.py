f8e0fa239c021951a9aeca427f071ba911d5ecc5
oeyh98 <skusus123@gmail.com>
2024-08-28 10:28:03 +0900

    solve 2/3

---
+++ 프로그래머스/lv1/양진혁/성격유형검사.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+def solution(survey, choices):
+    answer = ""
+    indicator = defaultdict(int)
+
+    score = [0, 3, 2, 1, 0, 1, 2, 3]
+
+    for i in range(len(survey)):
+        if choices[i] <= 4:
+            indicator[survey[i][0]] += score[choices[i]]
+        else:
+            indicator[survey[i][1]] += score[choices[i]]
+
+    answer += "R" if indicator["R"] >= indicator["T"] else "T"
+    answer += "C" if indicator["C"] >= indicator["F"] else "F"
+    answer += "J" if indicator["J"] >= indicator["M"] else "M"
+    answer += "A" if indicator["A"] >= indicator["N"] else "N"
+
+    return answer

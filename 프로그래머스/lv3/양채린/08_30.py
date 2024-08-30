5e9a2b29d72d770e86542d8515cd5a3b0f8f3324
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2024-08-30 16:00:50 +0900

    Day 54

--- 프로그래머스/lv3/양채린/08_30.py
+++ 프로그래머스/lv3/양채린/08_30.py
@@ -1,18 +1,24 @@
-def find()
+import sys
+sys.setrecursionlimit(10**6)
+
+
+def find(r_n, room_dict):
+    if r_n not in room_dict:
+        room_dict[r_n] = r_n + 1
+        return r_n
+    empty = find(room_dict[r_n], room_dict)
+    room_dict[r_n] = empty + 1
+    return empty
+
 
 def solution(k, room_number):
     answer = []
 
-    room = dict()
-
+    room_dict = dict()
     for r_n in room_number:
-        if r_n not in room.keys():
-            room[r_n] =
-
-
-
+        answer.append(find(r_n, room_dict))
 
     return answer
 
 # [1, 3, 4, 2, 5, 6]
-print(solution(10, [1, 3, 4, 1, 3, 1]))
+print(solution(10, [1, 3, 4, 1, 3, 1])) # 	[1, 3, 4, 2, 5, 6]

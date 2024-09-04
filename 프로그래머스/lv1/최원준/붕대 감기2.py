91c91fc691ca59e549e43994c6993cbc48806d6b
724thomas <724thomas@gmail.com>
2024-09-04 09:30:24 +0900

    Day 57

---
+++ 프로그래머스/lv1/최원준/붕대 감기2.py
@@ -0,0 +1,39 @@
+# https://www.acmicpc.net/problem/
+
+'''
+1. 아이디어 :
+
+2. 시간복잡도 :
+    O(
+3. 자료구조 :
+
+'''
+
+
+'''
+import java.util.*;
+class Solution {
+    static int time;
+    static int damage;
+    
+    public int solution(int[] bandage, int health, int[][] attacks) {
+        // x = heal, t = cont, y = additional_heal
+        int cont = bandage[0], heal = bandage[1], additional_health = bandage[2];
+        int curr_health = health;
+        int prev_time = 0;
+        
+        for (int i=0; i<attacks.length; i++){
+            time = attacks[i][0];
+            damage = attacks[i][1];
+            
+            int time_lapse = time - prev_time-1;
+            int additional_heal = ((time_lapse) / cont) * additional_health;
+            curr_health = Math.min(health, curr_health + (time_lapse * heal) + additional_heal);
+            curr_health -= damage;
+            if (curr_health<=0) return -1;
+            prev_time = time;
+        }
+        
+        return curr_health;
+    }
+}'''
---
+++ 프로그래머스/lv3/최원준/네트워크2.py
@@ -0,0 +1,51 @@
+# https://www.acmicpc.net/problem/
+
+'''
+1. 아이디어 :
+
+2. 시간복잡도 :
+    O(
+3. 자료구조 :
+
+'''
+
+
+'''
+import java.util.*;
+class Solution {
+    int[] par;
+    
+    int find(int x) {
+        if (x != par[x]) {
+            par[x] = find(par[x]);
+        } 
+        return par[x];
+    }
+    
+    void union(int a, int b){
+        int ra = find(a);
+        int rb = find(b);
+        par[rb] = ra;
+    }
+    
+    
+    public int solution(int n, int[][] computers) {
+        par = new int[n];
+        for (int i=0; i<n; i++){
+            par[i] = i;
+        }
+        int row = computers.length, col = computers[0].length;
+        for (int r=0; r<row; r++) {
+            for (int c=0; c<col; c++) {
+                if (computers[r][c] == 1) {
+                    union(r, c);
+                }
+            }
+        }
+        int ans = 0;
+        for (int i=0; i<n; i++){
+            ans += par[i] == i ? 1 : 0;
+        }
+        return ans;
+    }
+}'''

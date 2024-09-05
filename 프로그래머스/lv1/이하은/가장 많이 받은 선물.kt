1effdf5ed97b1047dea6837987c3b5ca52dc0e7c
leeeha <jxlhe46@gmail.com>
2024-09-05 10:29:27 +0900

    [이하은] Day58

---
+++ 프로그래머스/lv1/이하은/가장 많이 받은 선물.kt
@@ -0,0 +1,60 @@
+class Solution {
+    /**
+    더 많은 선물을 준 사람이 다음 달에 하나 받는다.
+    
+    선물 기록이 없거나 주고받은 수가 같다면,
+    선물 지수가 큰 사람이 하나 받는다.
+    선물 지수가 동일하면 선물 주고받지 않는다. 
+    
+    선물 지수 = give - take
+    
+    다음 달에 선물을 가장 많이 받을 친구의 선물 개수 
+    */
+    fun solution(friends: Array<String>, gifts: Array<String>): Int {
+        var answer: Int = 0
+        val N = friends.size 
+        
+        val hash = mutableMapOf<String, Int>()
+        for((idx, name) in friends.withIndex()){
+            hash[name] = idx
+        }
+        
+        val score = mutableMapOf<String, Int>()
+        val arr: MutableList<MutableList<Int>> =  MutableList(N){ MutableList(N) { 0 } }
+        
+        for(gift in gifts){
+            val temp = gift.split(" ")
+            val giveName = temp[0]
+            val takeName = temp[1]
+            
+            val i = hash[giveName] ?: 0
+            val j = hash[takeName] ?: 0
+            arr[i][j]++
+            
+            score[giveName] = (score[giveName] ?: 0) + 1
+            score[takeName] = (score[takeName] ?: 0) - 1
+        }
+        
+        // 최종 계산 
+        // 대각선 방향으로 선물 개수 비교 
+        val candidates = MutableList(N) { 0 }
+        for(i in arr.indices){
+            for(j in arr[i].indices){
+                if(arr[i][j] > arr[j][i]){
+                    
+                }else if(arr[i][j] > arr[j][i]){
+                    
+                }else{
+                    // 선물 지수 비교 
+                    if(score[] > score[]){
+                        
+                    }else{
+                        
+                    }
+                }
+            }
+        }
+        
+        return answer
+    }
+}
---
+++ 프로그래머스/lv2/이하은/JadenCase 문자열 만들기.kt
@@ -0,0 +1,37 @@
+class Solution {
+    fun solution(input: String): String {
+        var answer = ""
+        if(input.isBlank()) return input
+        
+        var idx = 0
+        while(idx < input.length){
+            val ch = input[idx]
+            if(ch == ' ') {
+                answer += ch
+                idx++
+                continue
+            }
+            
+            val spaceIdx = input.indexOf(" ", startIndex = idx)
+            var word = ""
+            if(spaceIdx == -1){
+                word = input.substring(idx)
+            }else{
+                word = input.substring(idx, spaceIdx)
+            }
+            
+            if(word.length == 1){
+                answer += word.toUpperCase()
+            }else{
+                val first = word[0].toUpperCase()
+                val second = word.substring(1).toLowerCase()
+                answer += (first + second)
+            }
+            
+            if(spaceIdx == -1) break
+            else idx = spaceIdx
+        }
+        
+        return answer
+    }
+}

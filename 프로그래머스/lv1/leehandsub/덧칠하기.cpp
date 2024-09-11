5a98fc76377f4ce293d51e5e48f33a79db1c0236
leehandsub <35682233+leehandsub@users.noreply.github.com>
2024-09-11 10:20:05 +0900

    Create 덧칠하기.cpp

---
+++ 프로그래머스/lv1/leehandsub/덧칠하기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, int m, vector<int> section) {
+    int answer = 0;
+    int now =0;
+    
+    for(int i=0;i<section.size();i++)
+    {
+        if(now<section[i])
+        {
+            now=section[i]+m-1;
+            answer++;
+        }
+    }
+    
+    
+    return answer;
+}

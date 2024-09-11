39571f0e5ee40817b900796dcbe21657669ba859
KangminLim <87487729+KangminLim@users.noreply.github.com>
2024-09-11 10:06:17 +0900

    [임강민] Day62/KM

---
+++ 프로그래머스/lv1/임강민/덧칠하기.py
@@ -0,0 +1,16 @@
+def solution(n, m, section):
+    answer = 0
+    p = [1] * (n + 1)
+    for s in section:
+        p[s] = 0
+
+    for s in section:
+        if p[s] == 1:
+            continue
+        else:
+            answer += 1
+            for i in range(s, s + m):
+                if i == n + 1: break
+                if p[i] == 0:
+                    p[i] = 1
+    return answer
---
+++ 프로그래머스/lv2/임강민/피보나치 수.py
@@ -0,0 +1,9 @@
+def solution(n):
+    answer = 0
+    F = [0] * (n + 1)
+    F[0], F[1] = 0, 1
+
+    for i in range(2, n + 1):
+        F[i] = F[i - 1] + F[i - 2]
+
+    return F[-1] % 1234567

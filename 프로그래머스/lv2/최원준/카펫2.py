06e4da143074677b3b00de8b2c335beea586014f
724thomas <724thomas@gmail.com>
2024-09-13 09:50:23 +0900

    Day 64

---
+++ 프로그래머스/lv2/최원준/카펫2.py
@@ -0,0 +1,30 @@
+# https://www.acmicpc.net/problem/
+
+'''
+1. 아이디어 :
+
+2. 시간복잡도 :
+    O(
+3. 자료구조 :
+
+import java.util.*;
+class Solution {
+    static ArrayList<int[]> getPair(int n){
+        var ans = new ArrayList<int[]>();
+        for (int i=1; i<=n/2; i++){
+            if (n%i == 0 && i+2 <= n/i+2){
+                ans.add(new int[]{n/i+2, i+2});
+            }
+        }
+        return ans;
+    }
+    public int[] solution(int brown, int yellow) {
+        if (yellow == 1) return new int[]{3,3};
+        for (int[] cords: getPair(yellow)){
+            if (cords[0] * 2 + (cords[1]-2) *2 == brown){
+                return new int[]{cords[0], cords[1]};
+            }
+        }
+        return new int[]{};
+    }
+}'''
---
+++ 프로그래머스/lv3/최원준/기지국 설치2.py
@@ -0,0 +1,34 @@
+# https://www.acmicpc.net/problem/
+
+'''
+1. 아이디어 :
+
+2. 시간복잡도 :
+    O(
+3. 자료구조 :
+
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[] stations, int w) {
+        int range = w * 2 + 1;
+        int ans = 0, count = 0;
+        int curr = 1;
+
+        for (int station : stations) {
+            int left = station - w;
+            if (curr < left) {
+                count = left - curr;
+                ans += count % range == 0 ? count/range : count/range+1;
+            }
+            curr = station + w + 1;
+        }
+
+        if (curr <= n) {
+            count = n - curr + 1;
+            ans += count % range == 0 ? count/range : count/range+1;
+        }
+
+        return ans;
+    }
+}'''

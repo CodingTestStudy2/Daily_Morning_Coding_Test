dba2234e91eca353500ebc032e65f7784c5da613
724thomas <724thomas@gmail.com>
2024-09-25 09:35:53 +0900

    Day 67

---
+++ 프로그래머스/lv1/최원준/기사단원의 무기2.py
@@ -0,0 +1,36 @@
+'''
+1. 아이디어 :
+
+2. 시간복잡도 :
+    O(
+3. 자료구조 :
+
+import java.util.*;
+class Solution {
+    public int get_power(int n, int limit, int power){
+        if (n==1) return 1;
+        int count = 0;
+        for (int i=1; i<=Math.sqrt(n); i++){
+            if (n % i == 0){
+                count += (i * i == n) ? 1 : 2;
+            }
+            if (count > limit){
+                return power;
+            }
+        }
+        return count;
+    }
+    public int solution(int number, int limit, int power) {
+        int ans = 0;
+        for (int i=1; i<=number; i++) {
+            ans += get_power(i, limit, power);
+        }
+        return ans;
+    }
+}
+
+
+
+'''
+
+
---
+++ 프로그래머스/lv3/최원준/불량 사용자2.py
@@ -0,0 +1,62 @@
+'''
+1. 아이디어 :
+
+2. 시간복잡도 :
+    O(
+3. 자료구조 :
+
+import java.util.*;
+class Solution {
+    Map<String, ArrayList<String>> candids;
+    Set<Set<String>> ans = new HashSet<>();
+
+    public boolean check(String a, String b){
+        if (a.length() != b.length()) return false;
+        for (int i=0; i<a.length(); i++){
+            if (a.charAt(i)=='*') continue;
+            if (a.charAt(i) != b.charAt(i)) return false;
+        }
+        return true;
+    }
+
+    public ArrayList<String> get_candids(String s, String[] user_ids){
+        ArrayList<String> candids = new ArrayList<>();
+        int n = s.length();
+        for (var user_id : user_ids){
+            if (check(s, user_id)) candids.add(user_id);
+        }
+        return candids;
+    }
+
+    public void combinations(ArrayList<ArrayList<String>> candidates, Set<String> currSet, int idx) {
+        if (idx == candidates.size()) {
+            ans.add(new HashSet<>(currSet));
+            return;
+        }
+
+        for (String user: candidates.get(idx)) {
+            if (!currSet.contains(user)) {
+                currSet.add(user);
+                combinations(candidates, currSet, idx+1);
+                currSet.remove(user);
+            }
+        }
+
+    }
+
+    public int solution(String[] user_ids, String[] banned_ids) {
+        ArrayList<ArrayList<String>> candidates = new ArrayList<>();
+        for (var banned_id: banned_ids){
+            candidates.add(get_candids(banned_id, user_ids));
+        }
+        combinations(candidates, new HashSet<>(), 0);
+        return ans.size();
+
+    }
+}
+
+
+
+'''
+
+

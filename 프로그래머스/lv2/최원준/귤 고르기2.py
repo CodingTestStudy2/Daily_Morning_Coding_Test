b2e58392de64263f2e5455ce220d9bdb0822f263
724thomas <724thomas@gmail.com>
2024-10-01 09:39:46 +0900

    Day 71

---
+++ 프로그래머스/lv2/최원준/귤 고르기2.py
@@ -0,0 +1,41 @@
+# https://www.acmicpc.net/problem/
+
+'''
+1. 아이디어 :
+
+2. 시간복잡도 :
+    O(
+3. 자료구조 :
+
+import java.util.*;
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        Map<Integer, Integer> counter = new HashMap<>();
+        for (int t : tangerine) {
+            counter.put(t, counter.getOrDefault(t, 0)+1);
+        }
+
+        int[][] arr = new int[counter.size()][2];
+        int idx = 0;
+        for (Map.Entry<Integer, Integer> entry : counter.entrySet()) {
+            arr[idx][0] = entry.getKey();
+            arr[idx][1] = entry.getValue();
+            idx++;
+        }
+        Arrays.sort(arr, (a,b) -> Integer.compare(b[1], a[1]));
+
+        int kinds = 0;
+        for (int[] count : arr) {
+            kinds ++;
+            k -= count[1];
+            if (k<=0) {
+                return kinds;
+            }
+        }
+        return arr.length;
+    }
+}
+
+'''
+
+
---
+++ 프로그래머스/lv3/최원준/섬 연결하기2.py
@@ -0,0 +1,57 @@
+# https://www.acmicpc.net/problem/
+
+'''
+1. 아이디어 :
+
+2. 시간복잡도 :
+    O(
+3. 자료구조 :
+
+import java.util.*;
+class Solution {
+// 5000 * 100
+    int[] par;
+    int[] rank;
+
+    public int find(int x) {
+        if (x != par[x]) par[x] = find(par[x]);
+        return par[x];
+    }
+
+    public boolean union(int a, int b){
+        int ra = find(a), rb = find(b);
+        if (ra == rb) return true;
+
+        if (rank[ra] > rank[rb]) {
+            par[rb] = ra;
+        } else if (rank[ra] < rank[rb]) {
+            par[ra] = rb;
+        } else {
+            par[rb] = ra;
+            rank[ra] ++;
+        }
+        return false;
+    }
+
+    public int solution(int n, int[][] costs) {
+        Arrays.sort(costs, (a, b) -> Integer.compare(a[2], b[2]));
+        int ans = 0, edges = 0;
+        par = new int[n];
+        rank = new int[n];
+        for (int i=0; i<n; i++) par[i] = i;
+
+        for (int[] cost : costs) {
+            int u = cost[0], v = cost[1], c = cost[2];
+            if (!union(u, v)){
+                ans += c;
+                edges ++;
+            }
+            if (edges == n-1) return ans;
+        }
+        return -1;
+    }
+}
+
+'''
+
+

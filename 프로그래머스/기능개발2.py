a5f9be43957be79048d46b7903e6ead30318284a
724thomas <724thomas@gmail.com>
2024-10-14 10:31:42 +0900

    Day 79

---
+++ 프로그래머스/기능개발2.py
@@ -0,0 +1,39 @@
+# https://www.acmicpc.net/problem/
+
+'''
+1. 아이디어 :
+
+2. 시간복잡도 :
+    O(
+3. 자료구조 :
+
+import java.util.*;
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int n = progresses.length;
+        ArrayList<Integer> jobs = new ArrayList<>();
+
+        int base = -1;
+        int count = 0;
+        for (int i=0; i<n; i++) {
+            int day = (int)Math.ceil((100.0-progresses[i]) / speeds[i]);
+            if (base < day) {
+                jobs.add(count);
+                base = day;
+                count = 1;
+            } else {
+                count ++;
+            }
+        }
+        jobs.add(count);
+        int[] ans = new int[jobs.size()-1];
+        for (int i=1; i<jobs.size(); i++){
+            ans[i-1] = jobs.get(i);
+        }
+        return ans;
+    }
+}
+
+'''
+
+
---
+++ 프로그래머스/숫자 문자열과 영단어2.py
@@ -0,0 +1,36 @@
+# https://www.acmicpc.net/problem/
+
+'''
+1. 아이디어 :
+
+2. 시간복잡도 :
+    O(
+3. 자료구조 :
+
+
+import java.util.*;
+class Solution {
+    public int solution(String s) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("zero", "0");
+        map.put("one", "1");
+        map.put("two", "2");
+        map.put("three", "3");
+        map.put("four", "4");
+        map.put("five", "5");
+        map.put("six", "6");
+        map.put("seven", "7");
+        map.put("eight", "8");
+        map.put("nine", "9");
+
+        for (var entry : map.entrySet()) {
+            s = s.replaceAll(entry.getKey(), entry.getValue());
+        }
+        return Integer.parseInt(s);
+    }
+
+}
+
+'''
+
+

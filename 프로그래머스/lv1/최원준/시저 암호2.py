e1050ed7672e893f8b79b1446a008e9f0a2e54fb
724thomas <724thomas@gmail.com>
2024-10-16 10:27:55 +0900

    Day 81

---
+++ 프로그래머스/lv1/최원준/시저 암호2.py
@@ -0,0 +1,29 @@
+# https://www.acmicpc.net/problem/
+
+'''
+1. 아이디어 :
+
+2. 시간복잡도 :
+    O(
+3. 자료구조 :
+
+class Solution {
+    public String solution(String s, int n) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isLowerCase(c)) {
+                sb.append((char) ((c - 'a' + n) % 26 + 'a'));
+            } else if (Character.isUpperCase(c)) {
+                sb.append((char) ((c - 'A' + n) % 26 + 'A'));
+            } else {
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
+    }
+}
+
+'''
+
+
---
+++ 프로그래머스/lv2/최원준/튜플2.py
@@ -0,0 +1,39 @@
+# https://www.acmicpc.net/problem/
+
+'''
+1. 아이디어 :
+
+2. 시간복잡도 :
+    O(
+3. 자료구조 :
+
+import java.util.*;
+class Solution {
+    public int[] solution(String s) {
+        s = s.replace("{{", "").replace("}}", "").replace("},{", " ");
+        String[] sets = s.split(" ");
+        List<String[]> setList = new ArrayList<>();
+        for (String set : sets) {
+            String[] numbers = set.split(",");
+            setList.add(numbers);
+        }
+        setList.sort(Comparator.comparingInt(arr -> arr.length));
+
+        Set<String> visited = new HashSet<>();
+        int[] ans = new int[setList.size()];
+        for (int i=0; i<setList.size(); i++){
+            for (String num : setList.get(i)) {
+                if (!visited.contains(num)){
+                    visited.add(num);
+                    ans[i] = Integer.parseInt(num);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
+
+'''
+
+

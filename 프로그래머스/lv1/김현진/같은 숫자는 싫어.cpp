8dd8103177256be4b70e9652706e9d002f33f0f6
Toothless <hyunjinney@naver.com>
2024-10-25 10:52:26 +0900

    [김현진] Day88

---
+++ 프로그래머스/lv1/김현진/같은 숫자는 싫어.cpp
@@ -0,0 +1,28 @@
+// 조건문으로 간단하게 구현
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) 
+{
+    vector<int> answer;
+    int num;
+    for(int i=0;i<arr.size();i++)
+    {
+        if(i==0){
+            answer.push_back(arr[i]);
+            num = arr[i];
+            continue;
+        }
+        if(num == arr[i]) continue;
+        else
+        {
+            answer.push_back(arr[i]);
+            num = arr[i];
+        }
+    }
+
+    return answer;
+}

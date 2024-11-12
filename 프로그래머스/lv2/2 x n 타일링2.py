43271d8934686c8c191c7e6f6faeebc8ddb4da58
724thomas <724thomas@gmail.com>
2024-11-12 10:31:06 +0900

    Day99

---
+++ 프로그래머스/lv2/2 x n 타일링2.py
@@ -0,0 +1,25 @@
+# https://www.acmicpc.net/problem/
+
+'''
+1. 아이디어 :
+
+2. 시간복잡도 :
+    O(
+3. 자료구조 :
+
+import java.util.*;
+class Solution {
+    //마지막 타일이 가로일떄 (n-2) , 세로일때 (n-1)
+    public int solution(int n) {
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i=3; i<=n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 1000000007;
+        }
+        return dp[n];
+    }
+}
+'''
+
+

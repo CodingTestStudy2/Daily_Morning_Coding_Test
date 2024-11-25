0ffba56d8d1f9360b28b79f1d035ce85e3204a2f
jaepyo-Lee <jaepyo99@naver.com>
2024-11-25 10:28:05 +0900

    PGS/LV3/코딩 테스트 공부(2)

---
+++ 프로그래머스/lv3/이재표/코딩 테스트 공부(2).java
@@ -0,0 +1,46 @@
+import java.util.*;
+class Solution {
+    public int solution(int alp, int cop, int[][] problems) {
+        int answer = 0;
+        int max_alp_req=0;
+        int max_cop_req=0;
+        for(int[]problem:problems){
+            max_alp_req=Math.max(problem[0],max_alp_req);
+            max_cop_req=Math.max(problem[1],max_cop_req);
+        }
+        if(max_alp_req<=alp && max_cop_req<=cop){ //현재
+            return 0;
+        }
+        if(alp>=max_alp_req){
+            alp=max_alp_req;
+        }
+        if(cop>=max_cop_req){
+            cop=max_cop_req;
+        }
+        int[][]dp=new int[max_alp_req+1][max_cop_req+1];
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
+                dp[i][j]=Integer.MAX_VALUE;
+            }
+        }
+        dp[alp][cop]=0;
+        for(int i=alp;i<=max_alp_req;i++){
+            for(int j=cop;j<=max_cop_req;j++){
+                if(i<max_alp_req){
+                    dp[i+1][j]=Math.min(dp[i+1][j],dp[i][j]+1);
+                }
+                if(j<max_cop_req){
+                    dp[i][j+1]=Math.min(dp[i][j+1],dp[i][j]+1);
+                }
+                for(int[]problem:problems){
+                    if(i>=problem[0] && j>=problem[1]){
+                        int new_alp=Math.min(i+problem[2],max_alp_req);
+                        int new_cop=Math.min(j+problem[3],max_cop_req);
+                        dp[new_alp][new_cop]=Math.min(dp[new_alp][new_cop],dp[i][j]+problem[4]);
+                    }
+                }
+            }
+        }
+        return dp[max_alp_req][max_cop_req];
+    }
+}

73a4c9db728e741f5c53cf7a1bdd616acda2c644
724thomas <724thomas@gmail.com>
2024-12-20 10:27:48 +0900

    Init

--- 프로그래머스/lv2/최원준/행렬의 곱셈.py
+++ 프로그래머스/lv2/최원준/행렬의 곱셈.py
@@ -1,23 +1,16 @@
-# https://www.acmicpc.net/problem/
-
-'''
-1. 아이디어 :
-    arr1의 row * arr2의 col 배열을 초기화한다.
-    arr1의 col(또는 arr2의 row)만큼 순회를 하면서 arr1, arr2의 곱한 값을 ans에 더한다.
-2. 시간복잡도 :
-    O(n * m * k)
-3. 자료구조 :
-    배열
-'''
-
+# https://school.programmers.co.kr/learn/courses/30/lessons/12949
 
 def solution(arr1, arr2):
-    r = len(arr1)
-    c = len(arr2[0])
-    ans = [[0 for _ in range(c)] for _ in range(r)]
-
-    for row in range(r):
-        for col in range(c):
-            for i in range(len(arr1[0])):
-                ans[row][col] += arr1[row][i] * arr2[i][col]
-    return ans
+    # arr1행과 arr2열 (arr1열과 arr2행은 같을테니)
+    n = len(arr1)
+    m = len(arr1[0])
+    k = len(arr2[0])
+    
+    answer = [[0] * k for _ in range(n)]
+    
+    for i in range(n):# arr1의 각 행
+        for l in range(k):# arr2의 각 열
+            for j in range(m):# arr1의 열과 arr2의 행 (공통 크기)
+                answer[i][l] += arr1[i][j] * arr2[j][l]
+    
+    return answer

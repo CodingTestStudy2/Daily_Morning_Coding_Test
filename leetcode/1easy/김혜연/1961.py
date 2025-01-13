561cd00b27df64111e7febeff1cfb0aac496de5f
Hyeyeon-Kim <clscls2530@naver.com>
2025-01-13 10:23:54 +0900

    [김혜연] DAY11

---
+++ leetcode/1easy/김혜연/1961.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPrefixString(self, s: str, words: List[str]) -> bool:
+        s2 = ""
+        i = 0
+
+        while len(s2) < len(s) and i < len(words):
+            s2 += words[i]
+            i += 1
+        
+        return s2 == s 

f46c3d645939c9422035d056ca9d7db587a88fcf
haneulOh <87457244+haneul5@users.noreply.github.com>
2025-01-13 10:32:13 +0900

    Create file : leet 1432

---
+++ leetcode/2medium/오하늘/1432.py
@@ -0,0 +1,23 @@
+# 에러 나서 찾는중
+class Solution:
+    def maxDiff(self, num: int) -> int:
+        num_str = str(num)
+        
+        for digit in num_str: # 최대
+            if digit != '9':
+                max_num = num_str.replace(digit, '9') # 9로 변경
+                break
+        else:
+            max_num = num_str
+        
+        if num_str[0] != '1': # 최소 첫번째는 1
+            min_num = num_str.replace(num_str[0], '1')
+        else:
+            for digit in num_str[1:]: 
+                if digit != '0' and digit != '1':
+                    min_num = num_str.replace(digit, '0')
+                    break
+            else:
+                min_num = num_str
+
+        return int(max_num) - int(min_num)

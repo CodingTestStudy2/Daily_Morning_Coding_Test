3fc9a71dab1721aecd10b9299210f19554f13524
박은비 <ebpark9511@gmail.com>
2025-01-16 10:28:18 +0900

    [박은비] Day14

---
+++ leetcode/1easy/박은비/1582.swift
@@ -0,0 +1,35 @@
+class Solution {
+    func numSpecial(_ mat: [[Int]]) -> Int {
+        // [[0,0,0,1],[1,0,0,0],[0,1,1,0],[0,0,0,0]]
+
+        // 행 1차원 배열
+        var rowSpecialCount = Array(repeating: 0, count: mat.count)
+        // 열 1차원 배열
+        var columnSpecialCount = Array(repeating: 0, count: mat[0].count)
+
+        // 등장횟수를 행, 열 1차원 배열에 카운팅
+        // rowSpecialCount: [1, 1, 2, 0]
+        // columnSpecialCount: [1, 1, 1, 1]
+        for i in 0..<mat.count {
+            for j in 0..<mat[i].count {
+                if mat[i][j] == 1 {
+                    rowSpecialCount[i] += 1
+                    columnSpecialCount[j] += 1
+                }
+            }
+        }
+
+        // 특별한 위치 찾기
+        var result = 0
+        for i in 0..<mat.count {
+            for j in 0..<mat[0].count {
+                // 1 등장위치 && 행 1차원 배열에 1번 등장 && 열 1차원 배열에 1번 등장
+                if mat[i][j] == 1 && rowSpecialCount[i] == 1 && columnSpecialCount[j] == 1 {
+                    result += 1
+                }
+            }
+        }
+
+        return result
+    }
+}

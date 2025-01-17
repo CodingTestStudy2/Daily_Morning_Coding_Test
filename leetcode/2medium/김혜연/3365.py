b99c6f20b1632d397f8260f4ec6a2b850e5e9fee
Hyeyeon-Kim <clscls2530@naver.com>
2025-01-17 10:40:46 +0900

    01-17

---
+++ leetcode/2medium/김혜연/3365.py
@@ -0,0 +1,38 @@
+class Solution:
+    def isPossibleToRearrange(self, s: str, t: str, k: int) -> bool:
+        slen = len(s)
+
+        l = 0
+        r = (slen)//k
+
+        d = {}
+
+        while(r <= slen):
+            substr = s[l:r]
+            
+            if substr in d:
+                d[substr] += 1
+            else :
+                d[substr] = 1
+
+            l += (slen)//k
+            r += (slen)//k
+
+        l = 0
+        r = (slen)//k
+
+        while(r <= slen):
+            substr = t[l:r]
+
+            if substr not in d :
+                return False
+
+            d[t[l:r]] -= 1
+
+            if d[substr] == 0:
+                del d[substr]
+
+            l += (slen)//k
+            r += (slen)//k
+
+        return 1

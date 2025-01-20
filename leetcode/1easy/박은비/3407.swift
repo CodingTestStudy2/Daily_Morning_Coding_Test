18d3d4a1e6ef3eb6d82cdf2267c2f2c5c59cb3b0
박은비 <ebpark9511@gmail.com>
2025-01-20 09:57:30 +0900

    [박은비] Day16

---
+++ leetcode/1easy/박은비/3407.swift
@@ -0,0 +1,36 @@
+/*
+p를 *기준으로 splited함
+
+
+1. s를 순회, 패턴이 일치하면 pIndex를 증가.
+2. pIndex == p.count 라면 true
+
+O(N)
+
+
+*/
+
+class Solution {
+    func hasMatch(_ s: String, _ p: String) -> Bool {
+        let splitedP: [String.SubSequence] = p.split(separator: "*")
+        var pIndex: Int = 0
+        var i: Int = 0
+        let sArr = Array(s)
+
+        while i < sArr.count && pIndex < splitedP.count {
+            guard i+splitedP[pIndex].count <= sArr.count else {
+                return false
+            }
+
+            let pattern = sArr[i..<i+splitedP[pIndex].count]
+            
+            if String(pattern) == String(splitedP[pIndex]) {
+                i += splitedP[pIndex].count
+                pIndex += 1
+            } else {
+                i += 1
+            }
+        }
+        return pIndex == splitedP.count
+    }
+}

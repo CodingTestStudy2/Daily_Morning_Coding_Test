774f827194c7ca5e4b8473328848719549555ed7
rudghgame <rudghgame@cau.ac.kr>
2025-02-05 10:31:24 +0900

    [이경호] Day25

---
+++ leetcode/1easy/이경호/3083.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+  bool isSubstringPresent(string s) {
+    for (int i = 0; i < s.length() - 1; i++) {
+      string temp = s.substr(i, 2);
+      reverse(temp.begin(), temp.end());
+      if (s.find(temp) != string::npos) {
+        return true;
+      }
+    }
+    return false;
+  }
+};

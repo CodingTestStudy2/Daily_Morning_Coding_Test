59c253492f86f23b674b372f7035287da973814e
haneulOh <87457244+haneul5@users.noreply.github.com>
2025-02-10 17:21:46 +0900

    Create code : leetcode 392

---
+++ leetcode/1easy/오하늘/392.py
@@ -0,0 +1,12 @@
+from collections import deque
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        s = deque(s)
+
+        for i in t:
+            if len(s) == 0: return True
+            if s[0] == i:
+                s.popleft()
+        if len(s) == 0: return True
+        else: return False

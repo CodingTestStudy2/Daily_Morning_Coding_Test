415262d9d22edbfd8f595d2bed5d07276a0ad460
박은비 <ebpark9511@gmail.com>
2025-02-11 10:31:33 +0900

    [박은비] Day30

---
+++ leetcode/1easy/박은비/121.swift
@@ -0,0 +1,21 @@
+class Solution {
+    func maxProfit(_ prices: [Int]) -> Int {
+        var left: Int = prices[0]
+        var right: Int = prices[0]
+        var maxProfit = 0
+
+        for i in 1..<prices.count {
+            
+            if left>prices[i] {
+                left = prices[i]
+                right = prices[i]
+            } else {
+                right = prices[i]
+                maxProfit = max(maxProfit, right-left)
+            }
+
+        }
+
+        return maxProfit
+    }
+}

e34fa55454c6e297c20dc9870610d419b3377bf7
박은비 <ebpark9511@gmail.com>
2025-02-13 10:32:00 +0900

    [박은비] Day31

---
+++ leetcode/1easy/박은비/1374.swift
@@ -0,0 +1,14 @@
+class Solution {
+    func generateTheString(_ n: Int) -> String {
+        
+        let count = n % 2 == 0
+        ? 1
+        : 0
+
+        var answer = String(repeating: "a", count: count)
+        answer += String(repeating: "z", count: n-count)
+
+        return answer
+
+    }
+}

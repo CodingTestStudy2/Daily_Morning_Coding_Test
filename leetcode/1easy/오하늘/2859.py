9800bfc4f3ddd009c966f1e4903de569583ffc6c
haneulOh <87457244+haneul5@users.noreply.github.com>
2025-02-14 10:29:31 +0900

    Create file : leet code 2859

---
+++ leetcode/1easy/오하늘/2859.py
@@ -0,0 +1,18 @@
+class Solution:
+    def sumIndicesWithKSetBits(self, nums: List[int], k: int) -> int:
+        ans = 0
+        def dfs(n, result):
+            if n == 0:
+                return
+            dfs(n // 2, result)
+            result.append(str(n % 2))  # 리스트에 추가
+
+        for i, n in enumerate(nums): # 배열이 아니라 인덱스를 이진법으로 구해야 함.
+            res = []
+            dfs(i, res)
+            result = ''.join(res)
+            print(result)
+            
+            if result.count('1') == k:
+                ans+=n
+        return ans

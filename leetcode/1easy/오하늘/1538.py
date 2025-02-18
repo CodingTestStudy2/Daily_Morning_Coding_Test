5cb816109b27cb94f0ce4d85f41617b7a2f77c07
haneulOh <87457244+haneul5@users.noreply.github.com>
2025-02-18 20:48:05 +0900

    Day34

---
+++ leetcode/1easy/오하늘/1538.py
@@ -0,0 +1,16 @@
+from collections import deque
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        arr = deque(arr)
+        tmp_k = 0
+        tmp = 1
+        while len(arr) > 0:
+            if tmp_k == k:
+                return tmp-1
+            if arr[0] == tmp: # 같으면
+                arr.popleft()
+                tmp+=1
+            else :
+                tmp+=1
+                tmp_k+=1
+        return tmp+(k-tmp_k)-1

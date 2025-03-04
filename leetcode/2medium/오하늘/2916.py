e06561b389870e69158f9fef91e80a8494437fd0
haneulOh <87457244+haneul5@users.noreply.github.com>
2025-03-04 10:25:42 +0900

    day44

---
+++ leetcode/2medium/오하늘/2916.py
@@ -0,0 +1,15 @@
+from itertools import combinations
+class Solution:
+    def lengthOfLongestSubsequence(nums, target) -> int:
+        # dp 같은데 구현 못했다..
+        nums.sort(reverse=True) # 큰 수부터 (최대를 구하니까)
+
+        ans = -1
+        for k in range(1, len(nums)+1):
+            for i in combinations(nums, k):
+                if sum(i) == target:
+                    if len(i)>ans: ans = len(i)
+
+        if ans == 0: return -1
+    
+    lengthOfLongestSubsequence([1000], 1000)

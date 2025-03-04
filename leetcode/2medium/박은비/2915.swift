ca5f6f27c0518ea9e470d2fe2e6af4fa17be0b4f
Evie <33455020+ebPark9511@users.noreply.github.com>
2025-03-04 10:18:34 +0900

    [박은비] Day44

---
+++ leetcode/2medium/박은비/2915.swift
@@ -0,0 +1,18 @@
+// dp[sum]=max(dp[sum],dp[sum−num]+1)
+class Solution {
+    func lengthOfLongestSubsequence(_ nums: [Int], _ target: Int) -> Int {
+        var dp = Array(repeating: -1, count: target + 1)
+        dp[0] = 0
+
+        for num in nums {
+            for sum in stride(from: target, through: num, by: -1) {
+                if dp[sum - num] != -1 {
+                    dp[sum] = max(dp[sum], dp[sum - num] + 1)
+                }
+            }
+        }
+
+        return dp[target]
+    }
+}
+

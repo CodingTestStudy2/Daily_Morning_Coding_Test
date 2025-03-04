214d45759609f1365f080e2270d81f07b03ee6fa
Evie <33455020+ebPark9511@users.noreply.github.com>
2025-03-04 10:17:52 +0900

    [박은비] Day44

---
+++ leetcode/1easy/박은비/1995.swift
@@ -0,0 +1,24 @@
+/*
+4 <= nums.length <= 50
+1 <= nums[i] <= 100
+*/
+class Solution {
+    func countQuadruplets(_ nums: [Int]) -> Int {
+        let n = nums.count
+        var count = 0
+
+        for a in 0..<n {
+            for b in (a+1)..<n {
+                for c in (b+1)..<n {
+                    for d in (c+1)..<n {
+                        if nums[a] + nums[b] + nums[c] == nums[d] {
+                            count += 1
+                        }
+                    }
+                }
+            }
+        }
+
+        return count
+    }
+}

bd6a772ae8410047aa386dc97f6cc9014f4f21ff
haneulOh <87457244+haneul5@users.noreply.github.com>
2025-03-06 10:28:19 +0900

    day46

---
+++ leetcode/2medium/오하늘/376.py
@@ -0,0 +1,22 @@
+class Solution:
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+        if not nums:
+            return 0       
+        n = len(nums)
+
+        # DP 구현
+        dp_up = [1]*n
+        dp_down = [1]*n
+
+        for i in range(1, n):
+            if nums[i] > nums[i-1]: # 증가
+                dp_up[i] = dp_down[i-1]+1
+                dp_down[i] = dp_down[i-1]
+            elif nums[i] < nums[i-1]: # 감소
+                dp_down[i] = dp_up[i-1] + 1
+                dp_up[i] = dp_up[i-1]
+            else: # 값이 같은 경우
+                dp_up[i] = dp_up[i-1]
+                dp_down[i] = dp_down[i-1]
+
+        return max(dp_up[-1], dp_down[-1])

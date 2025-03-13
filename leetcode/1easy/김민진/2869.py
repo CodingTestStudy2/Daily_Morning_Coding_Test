ed7e1b66030d29b9bddbd0daa2693b718b2c7a08
Minjin <tingst0208@naver.com>
2025-03-13 10:29:56 +0900

    Day51

---
+++ leetcode/1easy/김민진/2869.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        collection = [False] * k
+        count = 0
+        while sum(collection) != k:
+            top_num = nums.pop()  # 1 ~
+            count += 1
+            if top_num <= k:
+                collection[top_num - 1] = True
+        return count
+        

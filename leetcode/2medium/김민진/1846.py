7ff68ec816b3ed4b0e0867261c5f327c6c007d02
Minjin <tingst0208@naver.com>
2025-03-18 10:29:32 +0900

    Day54

---
+++ leetcode/2medium/김민진/1846.py
@@ -0,0 +1,10 @@
+import heapq
+class Solution:
+    def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
+        heapq.heapify(arr)  # min heap
+        N = len(arr)
+        heapq.heappop(arr)
+        curr_max = 1
+        for _ in range(1, N):
+            curr_max = min(heapq.heappop(arr), curr_max + 1)
+        return curr_max

ff449bf1628c1a1c341df3be62ac5eafd97deb6c
Minjin <tingst0208@naver.com>
2025-03-19 10:29:45 +0900

    Day55

---
+++ leetcode/2medium/김민진/2938.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minimumSteps(self, s: str) -> int:
+        count_swap = 0
+        first_1 = None
+        for i in range(len(s)):
+            if s[i] == "1":
+                first_1 = i
+                break
+        else:
+            return 0
+            
+        for i in range(first_1 + 1, len(s)):
+            if s[i] == "0":
+                count_swap += (i - first_1)
+                first_1 += 1
+        return count_swap

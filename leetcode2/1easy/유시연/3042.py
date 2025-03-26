415c337680a35ee2715d34acab2a7fa410064dec
siyeon yoo <84625956+syne77@users.noreply.github.com>
2025-03-26 10:30:21 +0900

    [유시연] Day03

---
+++ leetcode2/1easy/유시연/3042.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countPrefixSuffixPairs(self, words: List[str]) -> int:
+        count = 0
+        n = len(words)
+        
+        for i in range(n):
+            for j in range(i + 1, n):  # i < j 조건
+                len1, len2 = len(words[i]), len(words[j])
+                if len1 > len2:
+                    continue
+
+                if words[j][:len1] == words[i] and words[j][-len1:] == words[i]:
+                    count += 1
+        
+        return count          

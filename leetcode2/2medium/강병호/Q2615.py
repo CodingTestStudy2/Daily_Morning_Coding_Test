a85ce8602a153e65366c447ef5c24791691fe10a
Kang-bh <kabyho3513@gmail.com>
2025-03-26 10:32:02 +0900

    add 3/26

---
+++ leetcode2/2medium/강병호/Q2615.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+
+class Solution:
+    def distance(self, nums: list[int]) -> list[int]:
+        index_map = defaultdict(list)
+        for i, num in enumerate(nums):
+            index_map[num].append(i)
+        
+        n = len(nums)
+        arr = [0] * n
+        
+        for data in index_map.values():
+            m = len(data)
+            if m > 1:
+                prefix_sum = [0] * m
+                prefix_sum[0] = data[0]
+                
+                for i in range(1, m):
+                    prefix_sum[i] = prefix_sum[i - 1] + data[i]
+                
+                for i in range(m):
+                    left_sum = prefix_sum[i - 1] if i > 0 else 0
+                    right_sum = prefix_sum[-1] - prefix_sum[i]
+                    arr[data[i]] = (i * data[i] - left_sum) + (right_sum - (m - i - 1) * data[i])
+        
+        return arr
+

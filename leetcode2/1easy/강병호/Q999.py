b1dd57815989c0bddb44f368d5260f5e81ea9b56
Kang-bh <kabyho3513@gmail.com>
2025-03-27 10:40:04 +0900

    add 999.py

---
+++ leetcode2/1easy/강병호/Q999.py
@@ -0,0 +1,46 @@
+
+
+class Solution:
+    # 위 오 아 왼
+
+    def numRookCaptures(self, board: list[list[str]]) -> int:
+
+        dx = [0, 1, 0, -1]
+        dy = [1, 0, -1, 0]
+        result = 0
+        def findRookLocIdx():
+            for i in range(0, 8):
+                for j in range(0, 8):
+                    if board[i][j] == 'R':
+                        return i, j
+        
+        loc_x, loc_y = findRookLocIdx()
+        
+        for i in range(0, 4):
+            next_x, next_y = loc_x, loc_y
+            while True:
+                if next_x == -1 or next_x == 8 or next_y == -1 or next_y == 8:
+                    break
+                
+                if board[next_x][next_y] == 'B':
+                    break
+                elif board[next_x][next_y] == 'p':
+                    result+=1
+                    break
+
+                next_x += dx[i]
+                next_y += dy[i]
+        
+        print(result)
+
+        return result
+    
+
+
+
+if __name__ == "__main__":
+    sol = Solution()
+
+    sol.numRookCaptures([[".",".",".",".",".",".",".","."],[".",".",".","p",".",".",".","."],[".",".",".","R",".",".",".","p"],[".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".","."],[".",".",".","p",".",".",".","."],[".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".","."]])
+
+    

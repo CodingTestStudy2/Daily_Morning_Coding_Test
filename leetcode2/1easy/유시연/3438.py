285b1fb689701d5c7572a4cafcdfab5e41ed5b68
siyeon yoo <84625956+syne77@users.noreply.github.com>
2025-03-31 10:34:13 +0900

    [유시연] Day06

---
+++ leetcode2/1easy/유시연/3438.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findValidPair(self, s: str) -> str:
+        count = {}
+        for num in s:
+            if num in count:
+                count[num] += 1
+            else:
+                count[num] = 1
+
+        for i in range(len(s) -1):
+            num1, num2 = s[i], s[i + 1]
+
+            if num1 != num2 and count[num1] == int(num1) and count[num2] == int(num2):
+                return num1 + num2
+
+        return ""

90b786c16ce23a50e6a8dbb07bacac23dfc660e5
jaehoonlee <jaelee9212>
2025-04-08 21:29:58 -0400

    [이재훈] Day13

--- leetcode2/2medium/Q2957.java
+++ leetcode2/2medium/Q2957.java
@@ -14,14 +14,15 @@ public class Q2957 {
     class Solution {
         public int removeAlmostEqualCharacters(String word) {
 
-            int res =0;
+            int res = 0;
             for(int i = 0; i < word.length()-1; i++) {
-                System.out.println(word.charAt(i) - word.charAt(i+1));
-                if(Math.abs(word.charAt(i) - word.charAt(i+1)) <=1 ) {
+                //System.out.println(word.charAt(i) - word.charAt(i+1));
+                if(Math.abs(word.charAt(i) - word.charAt(i+1)) <=1) {
                     res++;
                     i++;
                 }
             }
+
             return res;
         }
     }

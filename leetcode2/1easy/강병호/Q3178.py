b39161414ba247e8afd728498d8c233f08087f80
Kang-bh <kabyho3513@gmail.com>
2025-04-09 10:35:27 +0900

    4/9

---
+++ leetcode2/1easy/강병호/Q3178.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numberOfChild(self, n: int, k: int) -> int:
+
+        time = 0
+        loc = 0
+        direction_right = True
+
+        while time != k:
+            if direction_right:
+                loc+=1
+            else:
+                loc-=1
+
+            time+=1
+
+            if loc == n-1:
+                direction_right = False
+            elif loc == 0: 
+                direction_right = True
+
+        return loc
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.numberOfChild(3, 5))
+    print(sol.numberOfChild(5, 6))
+    print(sol.numberOfChild(4, 2))

259e621bf518f7c45f1cffda852c6dd4c770ffef
siyeon yoo <84625956+syne77@users.noreply.github.com>
2025-04-09 10:33:21 +0900

    [유시연] Day 12

---
+++ leetcode2/1easy/유시연/3178.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numberOfChild(self, n: int, k: int) -> int:
+        i = -1
+        go_right = True
+
+        while k >= 0:
+            if i == 0:
+                go_right = True
+            if i == n-1:
+                go_right = False
+
+            if go_right:
+                i += 1
+                k -= 1
+
+            else:
+                i -= 1
+                k -= 1
+                
+        return i
+
+        

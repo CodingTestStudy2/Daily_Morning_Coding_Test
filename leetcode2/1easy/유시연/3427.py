35d0caa29dc7dd1032bc920e1899c75a82768e67
siyeon yoo <84625956+syne77@users.noreply.github.com>
2025-04-14 10:30:23 +0900

    [유시연] Day16

---
+++ leetcode2/1easy/유시연/3427.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subarraySum(self, nums: List[int]) -> int:
+        n = len(nums)
+    
+        prefix = [0] * (n + 1)
+        for i in range(n):
+            prefix[i + 1] = prefix[i] + nums[i]
+
+
+        total = 0
+        for i in range(n):
+            start = max(0, i - nums[i])
+            sub_sum = prefix[i + 1] - prefix[start]
+            total += sub_sum
+
+        return total

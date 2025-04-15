85d3e27ef121ebdedf2b6ed1d607170ee9e97064
Kang-bh <kabyho3513@gmail.com>
2025-04-15 09:28:48 +0900

    add 2908 by python

---
+++ leetcode2/1easy/강병호/Q2908.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minimumSum(self, nums: list[int]) -> int:
+        result = -1
+        for idx in range(1, len(nums)-1): # mid-num
+            left, right = 1, 1
+            while idx - left != -1:
+                # print(f"idx + right : {idx + right}")
+                # print(f"idx + left : {idx - left}")
+                if nums[idx] > nums[idx-left] and nums[idx] > nums[idx+right]:
+                    sumNum = nums[idx] + nums[idx-left] + nums[idx+right]
+                    if result == -1 or sumNum < result:
+                        result = sumNum
+                
+                if idx + right == len(nums) - 1:
+                    right = 1
+                    left += 1
+                else:
+                    right += 1
+                
+            
+        
+        return result
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.minimumSum([8, 6, 1, 5, 3]))
+    print(sol.minimumSum([6,5,4,3,4,5]))
+    print(sol.minimumSum([5,4,8,7,10,2]))

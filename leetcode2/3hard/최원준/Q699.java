7ad5b75dbedbdff3a307bedaca36081828a6b334
724thomas <724thomas@gmail.com>
2025-04-16 10:27:38 +0900

    Day18

--- leetcode2/3hard/최원준/Q699.java
+++ leetcode2/3hard/최원준/Q699.java
@@ -17,21 +17,11 @@ import java.util.List;
 import java.util.Map;
 
 public class Q699 {
-    class Pair {
-        int x;
-        int y;
-
-        public Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
     class Solution {
         public List<Integer> fallingSquares(int[][] positions) {
             int n = positions.length;
             List<Integer> ans = new ArrayList<>();
-            Map<Pair, Integer> squares = new HashMap<>(); //left, right : height
+            List<int[]> squares = new ArrayList<>();
 
             int curr = 0;
             for (int i=0; i<n; i++) {
@@ -39,39 +29,15 @@ public class Q699 {
                 int left = pos[0], right = pos[0] + pos[1]; //2, 5
 
                 int maxHeightInStack = pos[1];
-                Map<Pair, Integer> squares2 = new HashMap<>();
 
-                for (var square : squares.entrySet()) {
-                    Pair key = square.getKey();
-                    int sleft = key.x, sright = key.y, height = square.getValue(); // 1,3, 2
+                for (int[] square : squares) {
+                    int sleft = square[0], sright = square[1], height = square[2]; // 1,3, 2
 
-                    if (right <= sleft || sright <= left) {
-                        squares2.put(key, height);
-                        continue;
-                    }
-
-                    // 겹치는 애들은 다시 계산.
-                    // 겹치지 않는 부분은 그대로 냅둔다.
-                    maxHeightInStack = Math.max(maxHeightInStack, square.getValue() + pos[1]);
-                    if ( left <= sleft && sright <= right) { //감싸는 경우
-
-                    } else if (sleft <= left && right <= sright) { // 감싸지는 경우
-                        int newLeft1 = sleft, newRight1 = left;
-                        squares2.put(new Pair(newLeft1, newRight1), height);
-                        int newLeft2 = right, newRight2 = sright;
-                        squares2.put(new Pair(newLeft2, newRight2), height);
-                    } else if (left <= sleft && right <= sright) { // 왼쪽으로 더 긴 경우
-                        int newLeft2 = right, newRight2 = sright;
-                        squares2.put(new Pair(newLeft2, newRight2), height);
-                    } else if (sleft <= left && sright <= right) { // 오른쪽으로 더 긴 경우
-                        int newLeft1 = sleft, newRight1 = left;
-                        squares2.put(new Pair(newLeft1, newRight1), height);
-                    }
+                    if (right <= sleft || sright <= left) continue;
+                    maxHeightInStack = Math.max(maxHeightInStack, height + pos[1]);
                 }
-                squares2.put(new Pair(left, right), maxHeightInStack);
+                squares.add(new int[]{left, right, maxHeightInStack});
                 curr = Math.max(curr, maxHeightInStack);
-                squares = squares2;
-
                 ans.add(curr);
             }
             return ans;

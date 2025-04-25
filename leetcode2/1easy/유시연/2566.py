2ab5054d76d7e7b3ee02b3de7b359f90bbcd86b6
siyeon yoo <84625956+syne77@users.noreply.github.com>
2025-04-25 10:31:04 +0900

    [유시연]Day25

---
+++ leetcode2/1easy/유시연/2566.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minMaxDifference(self, num: int) -> int:
+        str_num = str(num)
+        max_val = num
+        min_val = num
+
+        for d in set(str_num):
+            if d != '9':
+                max_candidate = int(''.join('9' if ch == d else ch for ch in str_num))
+                max_val = max(max_val, max_candidate)
+
+        for d in set(str_num):
+            if d != '0':
+                min_candidate = int(''.join('0' if ch == d else ch for ch in str_num))
+                min_val = min(min_val, min_candidate)
+
+        return max_val - min_val

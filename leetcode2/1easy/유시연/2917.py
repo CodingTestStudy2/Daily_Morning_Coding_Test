1e0cabf31ec6dec158cdcf04909e5360aa04c45e
siyeon yoo <84625956+syne77@users.noreply.github.com>
2025-05-02 10:27:34 +0900

    [유시연] Day30

---
+++ leetcode2/1easy/유시연/2917.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findKOr(self, nums: List[int], k: int) -> int:
+        count = [0] * 32
+
+        for num in nums:
+            for i in range(32):
+                if num & (1 << i):
+                    count[i] += 1
+
+        result = 0
+        for i in range(32):
+            if count[i] >= k:
+                result |= (1 << i)
+
+        return result

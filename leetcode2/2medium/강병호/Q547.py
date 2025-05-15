3facb0f75d2e84a1be02363071b0dbf63a4a25b0
Kang-bh <kabyho3513@gmail.com>
2025-05-15 10:30:41 +0900

    add q547.py

---
+++ leetcode2/2medium/강병호/Q547.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findCircleNum(self, isConnected: list[list[int]]) -> int:
+
+        count = 0
+        visited = [False] * len(isConnected)
+
+        def dfs(c):
+            for adj_c in range(len(isConnected)):
+                if isConnected[c][adj_c] == 1 and not visited[adj_c]:
+                    visited[adj_c] = True
+                    dfs(adj_c)
+        
+        for c in range(len(isConnected)):
+            if not visited[c]:
+                visited[c] =True
+                dfs(c)
+                count += 1
+        
+        return count

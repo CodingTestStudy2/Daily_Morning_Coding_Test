9f0fb8ef814888ab3dc442c8a68e8916eb657f39
Kang-bh <kabyho3513@gmail.com>
2025-05-19 10:27:01 +0900

    add

---
+++ leetcode2/2medium/강병호/Q117.py
@@ -0,0 +1,24 @@
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return root
+        
+        current = root
+        temp = Node(0) 
+        tail = temp
+        
+        while current:
+            if current.left:
+                tail.next = current.left
+                tail = tail.next
+            if current.right:
+                tail.next = current.right
+                tail = tail.next
+            current = current.next
+            
+            if not current:
+                current = temp.next 
+                temp.next = None 
+                tail = temp
+        
+        return root

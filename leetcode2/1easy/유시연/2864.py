9793ccaf1cb3e2972ff238710c41e0e7da318688
siyeon yoo <84625956+syne77@users.noreply.github.com>
2025-05-22 10:30:59 +0900

    [유시연] Day19

---
+++ leetcode2/1easy/유시연/2864.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maximumOddBinaryNumber(self, s: str) -> str:
+        result = []
+        n = len(s)
+        count = 0
+
+        for i in range(n):
+            if s[i] == '1' and count == 0:
+                result.append('1')
+                count +=1
+
+            elif s[i] == '1' and count != 0:
+                result.insert(0,'1')
+            
+            elif s[i] == '0':
+                result.insert(len(result) - 1, '0')
+
+        return ''.join(result)

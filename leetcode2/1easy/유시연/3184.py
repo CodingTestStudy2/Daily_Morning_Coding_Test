3aa49d25fbbf65d0179ec10e6ed1a38d37d4bbae
siyeon yoo <84625956+syne77@users.noreply.github.com>
2025-05-23 10:31:37 +0900

    [유시연] Day20

---
+++ leetcode2/1easy/유시연/3184.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countCompleteDayPairs(self, hours: List[int]) -> int:
+        n = len(hours)
+        count = 0
+
+        for i in range(n):
+            for j in range(n):
+                if i <= j:
+                    continue
+                if (hours[i]+hours[j]) % 24 == 0:
+                    count += 1
+
+
+        return count

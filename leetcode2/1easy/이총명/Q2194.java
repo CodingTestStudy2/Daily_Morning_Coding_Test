5744736223de8d7b4d3ec2550d9eac83bb2e7021
CHONGMYOUNG LEE <53869165+dlchdaud123@users.noreply.github.com>
2025-05-27 10:10:46 +0900

    Create Q2194.java

---
+++ leetcode2/1easy/이총명/Q2194.java
@@ -0,0 +1,9 @@
+class Solution {
+    public List<String> cellsInRange(String s) {
+        String[] split_s = s.split(":");
+        List<String> split_s_List = Arrays.asList(split_s);
+        String letter1 = 
+        return split_s_List;
+
+    }
+}

529830081ad22bffcaecd134d090b8cdf2f24eb4
aiken516 <jimmy000516@naver.com>
2025-08-01 10:30:10 +0900

    [노형민] Day05

---
+++ leetcode2/1easy/노형민/Q1592.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+
+public class Solution
+{
+    public string ReorderSpaces(string text)
+    {
+        StringBuilder sb = new();
+        int spaceCount = 0;
+        int wordCount = 0;
+        bool inWord = false;
+
+        foreach (char c in text)
+        {
+            if (c == ' ')
+            {
+                spaceCount++;
+                inWord = false;
+            }
+            else
+            {
+                if (!inWord)
+                {
+                    wordCount++;
+                    inWord = true;
+                }
+            }
+        }
+
+        if (wordCount != 1)
+        {
+            bool wordStarted = true;
+            bool firstWord = true;
+            foreach (char c in text)
+            {
+                if (c == ' ')
+                {
+                    wordStarted = false;
+                }
+                else
+                {
+                    if (!wordStarted && !firstWord)
+                    {
+                        sb.Append(' ', spaceCount / (wordCount - 1));
+                    }
+
+                    sb.Append(c);
+                    wordStarted = true;
+                    firstWord = false;
+                }
+            }
+            
+            sb.Append(' ', spaceCount % (wordCount - 1));
+            
+            return sb.ToString();
+        }
+        else
+        {
+            sb.Append(text.Trim());
+            sb.Append(' ', spaceCount);
+            return sb.ToString();
+        }
+    }
+}

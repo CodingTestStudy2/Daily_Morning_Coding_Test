dd00c8007e57f67f0ff72cc13df8836f071fe895
aiken516 <jimmy000516@naver.com>
2025-08-04 10:30:05 +0900

    [노형민] Day06

---
+++ leetcode2/2medium/노형민/Q300.cs
@@ -0,0 +1,27 @@
+﻿public class Solution
+{
+    public int LengthOfLIS(int[] nums)
+    {
+        int result = 0;
+        int[] LISLength = new int[nums.Length];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (nums[i] > nums[j])
+                {
+                    LISLength[i] = Math.Max(LISLength[i], LISLength[j] + 1);
+                }
+            }
+
+            if (LISLength[i] > result)
+            {
+                result = LISLength[i];
+            }
+        }
+
+        //자기 자신이 포함되어야 하기에 + 1
+        return result + 1;
+    }
+}

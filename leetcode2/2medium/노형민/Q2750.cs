1c16cda956fac49370b08385f7c3667cbe6670b3
aiken516 <jimmy000516@naver.com>
2025-08-07 10:28:25 +0900

    [노형민] Day09

---
+++ leetcode2/2medium/노형민/Q2750.cs
@@ -0,0 +1,33 @@
+﻿public class Solution
+{
+    public int NumberOfGoodSubarraySplits(int[] nums)
+    {
+        const int modulo = 1_000_000_007;
+        int dividePoint = 0;
+        bool foundOne = false;
+
+        long result = 1;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == 1)
+            { 
+                foundOne = true;
+
+                result = (result * (dividePoint + 1)) % modulo;
+                dividePoint = 0;
+            }
+            else if (foundOne)
+            {
+                dividePoint++;
+            }
+        }
+
+        if (!foundOne)
+        {
+            return 0;
+        }
+
+        return (int)(result % modulo);
+    }
+}

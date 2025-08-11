0996c04e3154f5ebd3e09e442c3b022373e4d9b9
aiken516 <jimmy000516@naver.com>
2025-08-11 10:26:33 +0900

    [노형민] Day11

---
+++ leetcode2/1easy/노형민/Q3633.cs
@@ -0,0 +1,50 @@
+﻿public class Solution
+{
+    public int EarliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration)
+    {
+        int minTime = 10000;
+        for (int i = 0; i < landStartTime.Length; i++)
+        {
+            for (int j = 0; j < waterStartTime.Length; j++)
+            {
+                int currentTime = landStartTime[i] + landDuration[i];
+                if (waterStartTime[j] <= currentTime)
+                {
+                    currentTime += waterDuration[j];
+                }
+                else
+                {
+                    currentTime = waterStartTime[j] + waterDuration[j];
+                }
+
+                if (currentTime < minTime)
+                {
+                    minTime = currentTime;
+                }
+            }
+        }
+
+        for (int i = 0; i < waterStartTime.Length; i++)
+        {
+            for (int j = 0; j < landStartTime.Length; j++)
+            {
+                int currentTime = waterStartTime[i] + waterDuration[i];
+                if (landStartTime[j] <= currentTime)
+                {
+                    currentTime += landDuration[j];
+                }
+                else
+                {
+                    currentTime = landStartTime[j] + landDuration[j];
+                }
+
+                if (currentTime < minTime)
+                {
+                    minTime = currentTime;
+                }
+            }
+        }
+
+        return minTime;
+    }
+}

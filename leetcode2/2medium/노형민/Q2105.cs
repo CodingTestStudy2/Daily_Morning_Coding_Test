ec11bd4a741f4202464fadd9c9aa1c4f106fd145
aiken516 <jimmy000516@naver.com>
2025-08-14 10:30:11 +0900

    [노형민] Day14

---
+++ leetcode2/2medium/노형민/Q2105.cs
@@ -0,0 +1,58 @@
+﻿public class Solution
+{
+    public int MinimumRefill(int[] plants, int capacityA, int capacityB)
+    {
+        int ap = 0;
+        int bp = plants.Length - 1;
+
+        int aw = capacityA;
+        int bw = capacityB;
+
+        int refills = 0;
+
+        while (true)
+        {
+            if (ap > bp)
+            {
+                break;
+            }
+            if (ap == bp)
+            {
+                if (aw > bw && aw < plants[ap])
+                {
+                    refills++;
+                }
+                else if (bw > aw && bw < plants[bp])
+                {
+                    refills++;
+                }
+                else if (aw == bw && bw < plants[bp])
+                {
+                    refills++;
+                }
+
+                break;
+            }
+
+            if (aw < plants[ap])
+            {
+                refills++;
+                aw = capacityA;
+            }
+
+            aw -= plants[ap];
+            ap++;
+
+            if (bw < plants[bp])
+            {
+                refills++;
+                bw = capacityB;
+            }
+
+            bw -= plants[bp];
+            bp--;
+        }
+
+        return refills;
+    }
+}
---
+++ leetcode2/2medium/노형민/Q2947.cs
@@ -0,0 +1,33 @@
+﻿public class Solution
+{
+    public int BeautifulSubstrings(string s, int k)
+    {
+        int result = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            int vowelCount = 0;
+            int consonantCount = 0;
+            for (int j = i; j < s.Length; j++)
+            {
+                char c = s[j];
+                if ("aeiou".Contains(c))
+                {
+                    vowelCount++;
+                }
+                else
+                {
+                    consonantCount++;
+                }
+
+                if (vowelCount == consonantCount &&
+                    (vowelCount * consonantCount) % k == 0)
+                {
+                    result += 1;
+                }
+            }
+        }
+
+        return result;
+    }
+}

d569259d7ffa088f9f4e74109064edf8e619e1b9
aiken516 <jimmy000516@naver.com>
2025-08-15 10:26:24 +0900

    [노형민] Day15

---
+++ leetcode2/1easy/노형민/Q2614.cs
@@ -0,0 +1,44 @@
+﻿public class Solution
+{
+    public int DiagonalPrime(int[][] nums)
+    {
+        int maxPrime = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int diagonalValue1 = nums[i][i];
+            int diagonalValue2 = nums[i][nums.Length - 1 - i];
+
+            if (diagonalValue1 > maxPrime && IsPrime(diagonalValue1))
+            {
+                maxPrime = Math.Max(maxPrime, diagonalValue1);
+            }
+
+            if (diagonalValue2 > maxPrime && IsPrime(diagonalValue2))
+            {
+                maxPrime = Math.Max(maxPrime, diagonalValue2);
+            }
+        }
+
+        return maxPrime;
+    }
+
+    private bool IsPrime(int num)
+    {
+        int sqrt = (int)Math.Sqrt(num);
+        if (num == 1)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= sqrt; i++)
+        {
+            if (num % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ leetcode2/2medium/노형민/Q274.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public int HIndex(int[] citations)
+    {
+        citations = citations.OrderByDescending(c => c).ToArray();
+        int hIndex = 0;
+        for (int i = 0; i < citations.Length; i++)
+        {
+            if (citations[i] >= i + 1)
+            {
+                hIndex = i + 1;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return hIndex;
+    }
+}
